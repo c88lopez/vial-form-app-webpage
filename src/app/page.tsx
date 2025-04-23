caa0520f98808bb29dc4e5e5668babcf9430f504
Cristian Lopez <cristian@getrentcheck.com>
2025-04-22 21:27:11 -0300

    Added form page with list, dummy actions and create button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,120 @@
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+"use client";
 
+import { useEffect, useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  Copy,
+  Eye,
+  FilePlus,
+  MoreHorizontal,
+  Pencil,
+  Trash,
+} from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+type IForm = {
+  id: string;
+  name: string;
+  fields: Record<string, { type: string; question: string; required: boolean }>;
+};
+
+export default function Home() {
+  const [forms, setForms] = useState<IForm[]>([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    try {
+      const response = await fetch("http://localhost:8080/form");
+
+      const json = await response.json();
+
+      setForms(json.data ?? []);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  function handleViewForm(id: IForm["id"]) {}
+
+  function handleAddRecord(id: IForm["id"]) {}
+
+  return (
+    <div>
+      <div className="flex w-full items-center py-4 px-3">
+        <div className="flex w-full"></div>
+
+        <Button variant="default">Create Form</Button>
+      </div>
+
+      <div className="rounded-md border m-2">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Name</TableHead>
+              <TableHead className="text-right"></TableHead>
+            </TableRow>
+          </TableHeader>
+
+          <TableBody>
+            {forms.map((form) => (
+              <TableRow key={form.id}>
+                <TableCell className="font-medium">{form.name}</TableCell>
+                <TableCell className="text-right">
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button variant="ghost" className="h-8 w-8 p-0">
+                        <span className="sr-only">Open menu</span>
+                        <MoreHorizontal className="h-4 w-4" />
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                      <DropdownMenuLabel>Actions</DropdownMenuLabel>
+
+                      <DropdownMenuSeparator />
+
+                      <DropdownMenuItem
+                        onClick={() => {
+                          handleViewForm(form.id);
+                        }}
+                      >
+                        <Eye />
+                        View Form
+                      </DropdownMenuItem>
+                      <DropdownMenuItem
+                        onClick={() => {
+                          handleAddRecord(form.id);
+                        }}
+                      >
+                        <FilePlus />
+                        Add Record
+                      </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 }
--- src/components/app-breadcrumb.tsx
+++ src/components/app-breadcrumb.tsx
@@ -37,7 +37,7 @@ export default function AppBreadcrumb() {
     <Breadcrumb>
       <BreadcrumbList>
         <BreadcrumbItem className="hidden md:block">
-          <BreadcrumbLink href="#">Home</BreadcrumbLink>
+          <BreadcrumbLink href="#">Forms</BreadcrumbLink>
         </BreadcrumbItem>
         {title && (
           <>
